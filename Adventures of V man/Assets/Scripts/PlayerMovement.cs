d3b66fa237b1a119b450b2117e02135c1163aea7
Vasilis Ieropoulos <kirav96@gmail.com>
2019-04-01 23:17:16 +0100

    Added character animation and movement

---
+++ Adventures of V man/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	
+	public CharacterController2D controller;
+	public Animator animator;
+	public float runSpeed = 40f;
+	float horizontalMove = 0f; //float
+	bool jump = false ;
+	bool crouch = false ;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") *runSpeed;  //horizontal x runSpeed
+		
+		animator.SetFloat("Speed",Mathf.Abs(horizontalMove));
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
+		if (Input.GetButtonDown("Crouch"))   //creates the crouch function
+		{
+			crouch = true;
+		}
+		else if (Input.GetButtonUp("Crouch")) // releases Crouch
+		{
+			crouch = false;
+		}
+    }
+	
+	void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime,crouch,jump);
+		jump = false;
+		// stops jumping after the first time jump is pressed
+		//move character
+	}
+	
+}
