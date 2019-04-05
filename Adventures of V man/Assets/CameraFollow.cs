376f5ecc31be2a5e841f5d746f092eebbc85025b
Vasilis Ieropoulos <kirav96@gmail.com>
2019-04-05 14:43:41 +0100

    added moving baground and light

---
+++ Adventures of V man/Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+	
+	public float smoothSpeed = 0.125f;
+	public Vector3  offset;
+	void LateUpdate()
+	{
+		// every frame weget the position that we want to snap to and we use a vector
+		// to get a bit closerto that position and  how much closer we get depends on our 
+		// smoothness 
+		Vector3 desiredPosition = target.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition,smoothSpeed);
+		transform.position =smoothedPosition ;
+		
+		//transform.LookAt(target);
+	
+	}
+}
